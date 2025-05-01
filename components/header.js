1444a70cb30a6d4efd4eeb66eae296a6d3f02c9b
tuguldur0525 <76215892+tuguldur0525@users.noreply.github.com>
2025-05-02 01:43:40 +0800

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,10 +1,39 @@
-export default function Headers() {
-    return (
-        <section className="header">
-            <nav>
-                <a href="index.js"> <img src="/images/Logo_AN.png"></img></a>
-                
-            </nav>
-        </section>                 
-    )
+// components/Header.js
+
+'use client'; // хэрэгтэй, учир нь onclick handler байгаа
+
+import { useEffect } from 'react';
+
+export default function Header() {
+  useEffect(() => {
+    const navLinks = document.getElementById("navLinks");
+
+    window.showMenu = () => {
+      if (navLinks) navLinks.style.right = "0";
+    };
+
+    window.hideMenu = () => {
+      if (navLinks) navLinks.style.right = "-200px";
+    };
+  }, []);
+
+  return (
+    <section className="header">
+      <nav>
+        <a href="/">
+          <img src="/images/Logo_AN.png" alt="Logo" />
+        </a>
+        <div className="nav-links" id="navLinks">
+          <i className="fa fa-times" onClick={() => window.hideMenu()}></i>
+          <ul>
+            <li><a href="/">Нүүр</a></li>
+            <li><a href="/buh_zaruud">Бүх зарууд</a></li>
+            <li><a href="/sell">Зар нэмэх</a></li>
+            <li><a href="/login" className="nevtreh-btn">Нэвтрэх</a></li>
+          </ul>
+        </div>
+        <i className="fa fa-bars" onClick={() => window.showMenu()}></i>
+      </nav>
+    </section>
+  );
 }
